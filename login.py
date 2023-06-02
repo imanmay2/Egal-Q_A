0015d14b7febc29b5f739805789f2a6dd5e87301
Manmay <manmaychakarborty@gmail.com>
2023-06-02 22:01:21 +0530

    login page updated(bg added)

--- login.py
+++ login.py
@@ -1,2 +1,13 @@
+from tkinter import *
 def login():
-    
+    from PIL import Image, ImageTk
+    win=Tk()
+    win.maxsize(width=1000,height=600)
+    win.minsize(width=1000,height=600)
+    bg = ImageTk.PhotoImage(file="bg1.PNG")
+    canvas = Canvas(win,width=1000, height=600)
+    canvas.pack(fill= "both", expand=True)
+    canvas.create_image(0, 0, image=bg, anchor="nw")
+    win.mainloop()
+
+login()
