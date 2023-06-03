8409c9ca61b7904eab7c953898dc538894c81c0e
Manmay <manmaychakarborty@gmail.com>
2023-06-03 09:16:45 +0530

    add_Q (basic structure done)

--- add_Q.py
+++ add_Q.py
@@ -1,2 +1,15 @@
+from tkinter import *
 def add_Q():
-    pass
+    win=Tk()
+    win.maxsize(width=1000,height=500)
+    win.minsize(width=1000,height=500)
+    from PIL import Image, ImageTk
+    canvas = Canvas(win,width=1000, height=600)
+    canvas.pack(fill= "both", expand=True)
+    img= (Image.open("Images/bg4.JPEG"))
+    resized_image= img.resize((1000,600), Image.ANTIALIAS)
+    new_image= ImageTk.PhotoImage(resized_image)
+    bg = ImageTk.PhotoImage(resized_image)
+    canvas.create_image(0, 0, image=bg, anchor="nw")
+    win.mainloop()
+add_Q()
